2f3b7b2ef1ee09ff88bb6ed430de1bc02ac4ac4c
Jareer Zeenam <jareerzeenam.28@gmail.com>
2023-11-19 23:49:16 +0000

    Style updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <section class="container mx-auto px-6 h-full flex flex-col">
+    <section class="container mx-auto px-6 h-full flex flex-col" style="max-width: 1000px;">
             <header class="flex justify-center print:justify-start">
                 <h1>
                     <img src="/logo.svg" alt="Code Breaker" aria-label="Code Breaker" class="print:invert print:w-36">
